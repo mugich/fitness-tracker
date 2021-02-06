7194491e966725bf8ab9786874d5b871a9b99dde
mugi <munkhuu1233@gmail.com>
2021-02-06 18:27:04 -0500

    added post and put api routes

--- routes/api.js
+++ routes/api.js
@@ -11,9 +11,28 @@ app.get("/api/workouts", (req, res) => {
 });
 
 
+app.post("/api/workouts", (req, res) => {
+  db.Workout.create({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
-
-
-
+app.put("/api/workouts/:id", ({ body, params }, res) => {
+  db.Workout.findByIdAndUpdate(
+    params.id,
+    {$push: { exercises: body }} 
+ 
+  )
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
 };
