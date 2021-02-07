da7bcd83058ac650c30a24c9a036c3b4c9c5074f
mugi <munkhuu1233@gmail.com>
2021-02-06 19:47:48 -0500

    added total duration

--- models/workout.js
+++ models/workout.js
@@ -32,8 +32,19 @@ const WorkoutSchema = new Schema({
     },
   ],
 },
+{
+  toJSON: {
+    virtuals: true
+  }
+}
 
 );
+  WorkoutSchema.virtual("totalDuration").get(function () {
+
+    return this.exercises.reduce((total, exercise) => {
+      return total + exercise.duration;
+    }, 0);
+  });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 module.exports = Workout;
