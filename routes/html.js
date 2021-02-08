88ae5b36ccaa578dc164de37b727abb2d3270919
mugi <munkhuu1233@gmail.com>
2021-02-08 17:56:12 -0500

    added seeders/seed.js

--- routes/html.js
+++ routes/html.js
@@ -1,13 +1,11 @@
-  const path = require("path");
+const path = require("path");
 
+module.exports = function (app) {
+  app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
 
-  module.exports = function(app) { 
-app.get("/exercise", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/exercise.html"));
-});
-
-app.get("/stats", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/stats.html"));
-});
-
-  };
+  app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
+};
